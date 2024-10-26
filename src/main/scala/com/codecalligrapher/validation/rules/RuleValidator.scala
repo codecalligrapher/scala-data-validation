d9fe87f3987fe05e5572ecf181c1f0fc889e5688
Aadidev Sooknanan <a.aadi380@gmail.com>
2024-10-26 09:58:32 -0400

    add model, rules

---
+++ src/main/scala/com/codecalligrapher/validation/rules/RuleValidator.scala
@@ -0,0 +1,32 @@
+package com.example.validation.rules
+
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.functions._
+import com.example.validation.model.ValidationRule
+
+trait RuleValidator {
+  def validate(df: DataFrame, rule: ValidationRule): DataFrame
+}
+
+class NotNullValidator extends RuleValidator {
+  override def validate(df: DataFrame, rule: ValidationRule): DataFrame = {
+    df.withColumn(
+      s"${rule.column}_validation",
+      when(col(rule.column).isNull, "FAILED")
+        .otherwise("PASSED")
+    )
+  }
+}
+
+class RangeValidator extends RuleValidator {
+  override def validate(df: DataFrame, rule: ValidationRule): DataFrame = {
+    val min = rule.parameters("min").toDouble
+    val max = rule.parameters("max").toDouble
+    
+    df.withColumn(
+      s"${rule.column}_validation",
+      when(col(rule.column).between(min, max), "PASSED")
+        .otherwise("FAILED")
+    )
+  }
+}
